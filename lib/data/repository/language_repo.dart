9f0d9fd2daf1e536f4c978b47e733e7b5a4b3925
AkhilPerumal <akhil.perumal.m.v@gmail.com>
2022-09-16 11:32:13 +0530

    initial commit

---
+++ lib/data/repository/language_repo.dart
@@ -0,0 +1,9 @@
+import 'package:carclenx_vendor_app/data/model/response/language_model.dart';
+import 'package:carclenx_vendor_app/util/app_constants.dart';
+import 'package:flutter/material.dart';
+
+class LanguageRepo {
+  List<LanguageModel> getAllLanguages({BuildContext context}) {
+    return AppConstants.languages;
+  }
+}
---
+++ lib/helper/get_di.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:carclenx_vendor_app/controller/auth_controller.dart';
+import 'package:carclenx_vendor_app/controller/language_controller.dart';
+import 'package:carclenx_vendor_app/controller/localization_controller.dart';
+import 'package:carclenx_vendor_app/controller/notification_controller.dart';
+import 'package:carclenx_vendor_app/controller/order_controller.dart';
+import 'package:carclenx_vendor_app/controller/splash_controller.dart';
+import 'package:carclenx_vendor_app/controller/theme_controller.dart';
+import 'package:carclenx_vendor_app/data/repository/auth_repo.dart';
+import 'package:carclenx_vendor_app/data/repository/language_repo.dart';
+import 'package:carclenx_vendor_app/data/repository/notification_repo.dart';
+import 'package:carclenx_vendor_app/data/repository/order_repo.dart';
+import 'package:carclenx_vendor_app/data/repository/splash_repo.dart';
+import 'package:carclenx_vendor_app/data/api/api_client.dart';
+import 'package:carclenx_vendor_app/util/app_constants.dart';
+import 'package:carclenx_vendor_app/data/model/response/language_model.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get/get.dart';
+
+Future<Map<String, Map<String, String>>> init() async {
+  // Core
+  final sharedPreferences = await SharedPreferences.getInstance();
+  Get.lazyPut(() => sharedPreferences);
+  Get.lazyPut(() => ApiClient(
+      appBaseUrl: AppConstants.BASE_URL, sharedPreferences: Get.find()));
+
+  // Repository
+  Get.lazyPut(
+      () => SplashRepo(sharedPreferences: Get.find(), apiClient: Get.find()));
+  Get.lazyPut(() => LanguageRepo());
+  Get.lazyPut(
+      () => AuthRepo(apiClient: Get.find(), sharedPreferences: Get.find()));
+  Get.lazyPut(
+      () => OrderRepo(apiClient: Get.find(), sharedPreferences: Get.find()));
+  Get.lazyPut(() =>
+      NotificationRepo(apiClient: Get.find(), sharedPreferences: Get.find()));
+
+  // Controller
+  Get.lazyPut(() => ThemeController(sharedPreferences: Get.find()));
+  Get.lazyPut(() => SplashController(splashRepo: Get.find()));
+  Get.lazyPut(() => LocalizationController(
+      sharedPreferences: Get.find(), apiClient: Get.find()));
+  Get.lazyPut(() => LanguageController(sharedPreferences: Get.find()));
+  Get.lazyPut(() => AuthController(authRepo: Get.find()));
+  Get.lazyPut(() => OrderController(orderRepo: Get.find()));
+  Get.lazyPut(() => NotificationController(notificationRepo: Get.find()));
+
+  // Retrieving localized data
+  Map<String, Map<String, String>> _languages = Map();
+  for (LanguageModel languageModel in AppConstants.languages) {
+    String jsonStringValues = await rootBundle
+        .loadString('assets/language/${languageModel.languageCode}.json');
+    Map<String, dynamic> _mappedJson = json.decode(jsonStringValues);
+    Map<String, String> _json = Map();
+    _mappedJson.forEach((key, value) {
+      _json[key] = value.toString();
+    });
+    _languages['${languageModel.languageCode}_${languageModel.countryCode}'] =
+        _json;
+  }
+  return _languages;
+}
